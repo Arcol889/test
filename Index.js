78ff1c61b0b3363f8d1a247bda5d3acbd969d7f2
Arcol889 <58913041+Arcol889@users.noreply.github.com>
2020-01-10 23:00:25 +0100

    Add files via upload

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
 const embedRouge = 0x1E90FF
 const embedbleu = 0x0064ff
-bot.login("NjY0ODAzNDUyMTI3MjgxMTgy.XhcYxQ.8SHB1erMhgRmC-BJg35JNLuSwzs")
+bot.login(process.env.TOKEN)
 const_embed = new Discord.RichEmbed()
 const prefix = '+'
 const PREFIX = '+'
@@ -211,85 +211,3 @@ bot.on('message', msg => {
   }
 });
  
-// ذا كود الباك اب
-bot.on('message', msg => {
-  if(msg.author.bot) return
-  if(msg.content.startsWith(prefix + 'backup')) {
-    db.set(`backup.${msg.author.id}.channels`, [])
-    db.set(`backup.${msg.author.id}.roles`, [])
-    db.set(`backup.${msg.author.id}.categories`, [])
-    let channels = msg.guild.channels.filter(c => c.type === 'text')
-    let categories = msg.guild.channels.filter(c => c.type === 'category')
-    channels.forEach(c => {
-      db.push(`backup.${msg.author.id}.channels`, {cn: c.name, ccn: c.parent.name})
- 
-    })
-    categories.forEach(c => {
-      db.push(`backup.${msg.author.id}.categories`, c.name)
- 
-    })
- 
-    msg.guild.roles.forEach(r => {
-      if(r.name === '@everyone') return
-      db.push(`backup.${msg.author.id}.roles`, {rn: r.name, rc: r.color, rp: r.permissions})
-    })
- 
-    msg.channel.send(`**Done backup this server**`)
- 
-  }
-})
- 
-
- 
- 
-
-bot.on('message', msg => {
-  if(msg.author.bot) return
-  if(msg.content.startsWith(prefix + 'load')) {
-   let channels = db.get(`backup.${msg.author.id}.channels`)
-   let roles = db.get(`backup.${msg.author.id}.roles`)
-   let categories = db.get(`backup.${msg.author.id}.categories`)
-   if(channels === null && roles === null && categories === null) return msg.channel.send(`**You don't have a backup to be uploaded here.  :/**`)
-  msg.channel.send(`**loading...**`).then(m => {
-    setTimeout(() => {
-                   m.edit(`**done load!**`)
-                 },6000);
-               })
- 
-if(categories != null) {
-  for(let j = 0; j < categories.length; j++) {
-    msg.guild.createChannel(categories[j], "category")
-  }
-}
-if(roles != null) {
-for(let r = 0; r < roles.length; r++) {
-msg.guild.createRole({
-  name: roles[r].rn,
-  color: roles[r].rc,
-  permissions: roles[r].rp
-})
-}
-}
-if(channels != null) {
- 
-  for(let i = 0; i < channels.length; i++) {
-    msg.guild.createChannel(channels[i].cn, "text").then(channel => {
-      channel.setParent(msg.guild.channels.find(c => c.name == channels[i].ccn))
-    })
-}
-}
- }
-});
-client.on('message', msg => {
-  if(message.content.startsWith(`${prefix}Avatar`)) {
-   
-    let user = message.mentions.users.first() ? message.mentions.users.first() : message.author
-      let ava = user.displayAvatarURL
-      let embed = {
-          color:0x08c3f3,
-          description:"**Voici l'avatar de : "+user.username+" ** [   󠇰󠇰󠇰󠇰 ]("+ava+")",
-          image:{url:ava}
-      }
-      message.channel.send("", {embed});
-    }
-  });
